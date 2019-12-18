f07adc7e543f7434aa35bdacf6a9c101f148e68f
essarraj.fouad <essarraj.fouad@gmail.com>
2019-12-18 12:44:37 +0100

    Ajoutez des fichiers projet.

---
+++ Bonjour_Chsarp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonjour_Chsarp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Saisie de votre Nom : 
+            string Nom;
+            Console.Write("Donnez votre Nom :");
+            Nom = Console.ReadLine();
+
+            Console.WriteLine("Votre Nom :" + Nom);
+
+
+            // Pour arrêter la fermeture du console.
+            Console.ReadKey();
+        }
+    }
+}
